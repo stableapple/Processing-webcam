5b59ef1a47eb65618e8b83a5d66aa7d7326cbe3a
stableapple <riddhimenon@am.students.amrita.edu>
2020-06-03 17:22:29 +0530

    Removed files

--- mysite/blog/urls.py
+++
@@ -1,7 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path('', views.index.as_view(), name='home'),
-    path('<slug:slug>/', views.view_post.as_view(), name='view_post'),
-]
